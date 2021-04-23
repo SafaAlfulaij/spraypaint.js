f6af4f6004a81ae083ec07709049e511c2a2fb48
Safa AlFulaij <salfulaij@lumofy.com>
2021-04-23 19:31:02 +0300

    Fix config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { readFileSync } from "fs"
-import uglify from "rollup-plugin-uglify"
-import commonjs from "rollup-plugin-commonjs"
-import resolve from "rollup-plugin-node-resolve"
+import { terser } from "rollup-plugin-terser"
+import commonjs from "@rollup/plugin-commonjs"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 
 const pkg = require("./package.json")
 
@@ -16,9 +16,9 @@ const banner = readFileSync("banner.js", "utf-8")
 export default {
   input: pkg.module,
   plugins: [
-    isProduction ? uglify({}) : {},
-    resolve(),
-    commonjs({ include: "./node_modules/**" }),
+    isProduction ? terser({}) : {},
+    nodeResolve(),
+    commonjs({ include: "./node_modules/**" })
   ],
   banner: banner,
   sourceMap: false,
@@ -26,7 +26,8 @@ export default {
   output: [
     {
       file: `./dist/${pkg.name}.${isProduction ? "min.js" : "js"}`,
-      format: "umd",
-    },
-  ],
+      name: `${pkg.name}`,
+      format: "umd"
+    }
+  ]
 }
